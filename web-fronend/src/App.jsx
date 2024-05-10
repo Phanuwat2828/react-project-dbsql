add05ba56b13414cc50fbba7ebb65c319a2b458d
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-05-11 00:11:17 +0700

    update connect api with database

--- web-fronend/src/App.jsx
+++ web-fronend/src/App.jsx
@@ -3,24 +3,24 @@ import React , {useState, useEffect} from "react";
 
 
 export default function App(){
+  const url = import.meta.env.VITE_API;
   const [users, setUsers] = useState([])
   useEffect(() =>{
-    fetch("http://localhost:5000/users")
-    // .than(res => {
-    //   if (!res.ok) {
-    //     throw new Error('Network response was not ok');
-    //   }
-    //   return res.json();
-    // })
-    // .than(result =>{
-    //   console.log(result);
-    // })
+    fetch(url)
+      .then(res => res.json())
+      .then(result =>{
+        setUsers(result);
+        console.log(result);
+      })
   },[])
   return(
     <div className="test">
       <h2 className="txt">
-        Hello World!
+          Data_user
       </h2>
+      {users.map(user =>(
+        <p>{user.id_account} {user.username_account} {user.email_account}</p>
+      ))}
     </div>
   )
 }
